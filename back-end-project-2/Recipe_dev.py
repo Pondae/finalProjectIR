c80bb7e02005bcefb6f2bd901a3d51b0bb54b411
PasakonPJ <paskon_paingjai@cmu.ac.th>
2022-03-02 17:20:37 +0700

    edit tfidf

--- back-end-project-2/Recipe_dev.py
+++ back-end-project-2/Recipe_dev.py
@@ -21,22 +21,21 @@ data['Cleaned_Ingredients'] = data['Cleaned_Ingredients'].apply(
 data['Cleaned_Ingredients'] = data['Cleaned_Ingredients'].apply(lambda s: s.lower())
 data['Cleaned_Ingredients'] = data['Cleaned_Ingredients'].drop_duplicates()
 
-print(len(data))
-print(len(data['Title']))  # Title
-print(len(data['Cleaned_Ingredients']))  # Ingredient
+# print(len(data))
+# print(len(data['Title']))  # Title
+# print(len(data['Cleaned_Ingredients']))  # Ingredient
 
-tfidf = TfidfVectorizer()
-Title_vector = tfidf.fit_transform(data['Title'].astype('U'))
-Ingredients_vector = tfidf.fit_transform(data['Cleaned_Ingredients'].astype('U'))
+tfidf_Title_vector = TfidfVectorizer()
+tfidf_Ingredients_vector = TfidfVectorizer()
+Title_vector = tfidf_Title_vector.fit_transform(data['Title'].astype('U'))
+Ingredients_vector = tfidf_Ingredients_vector.fit_transform(data['Cleaned_Ingredients'].astype('U'))
 
 
 def SearchingByTitle(query):
-    query = 'Miso-Butter Roast Chicken With Acorn Squash Panzanella'
-    query_vec = tfidf.transform([query])
+    query_vec = tfidf_Title_vector.transform([query])
     results = cosine_similarity(Title_vector, query_vec).reshape((-1,))
     output = []
     for i in results.argsort()[-10:][::-1]:
-        print(i)
         output.append(
             {"Title": data.iloc[i, 0], "Recipe": data.iloc[i, 2]
              }
@@ -45,7 +44,7 @@ def SearchingByTitle(query):
 
 
 def SearchingByIngredients(query):
-    query_vec = tfidf.transform([query])
+    query_vec = tfidf_Ingredients_vector.transform([query])
     results = cosine_similarity(Ingredients_vector, query_vec).reshape((-1,))
     output = []
     for i in results.argsort()[-20:][::-1]:
